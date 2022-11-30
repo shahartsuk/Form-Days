cf54d90debabace451ef4291837e4556b75d3880
Shahar <shahartzuk23@gmail.com>
2022-11-30 14:07:15 +0200

    Forms

---
+++ Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace WindowsFromsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.DayBox = new System.Windows.Forms.TextBox();
+            this.AgeBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 236);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // NameBox
+            // 
+            this.NameBox.Location = new System.Drawing.Point(147, 233);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(100, 20);
+            this.NameBox.TabIndex = 1;
+            this.NameBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.NameBox_MouseClick);
+            // 
+            // DayBox
+            // 
+            this.DayBox.Location = new System.Drawing.Point(147, 279);
+            this.DayBox.Name = "DayBox";
+            this.DayBox.Size = new System.Drawing.Size(100, 20);
+            this.DayBox.TabIndex = 2;
+            this.DayBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.DayBox_MouseClick);
+            // 
+            // AgeBox
+            // 
+            this.AgeBox.Location = new System.Drawing.Point(147, 332);
+            this.AgeBox.Name = "AgeBox";
+            this.AgeBox.Size = new System.Drawing.Size(100, 20);
+            this.AgeBox.TabIndex = 3;
+            this.AgeBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.AgeBox_MouseClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(84, 279);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Day";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(84, 332);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Age";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(545, 275);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 58);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "AddToFile";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(545, 154);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(177, 62);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Load";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.label4.Location = new System.Drawing.Point(147, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 31);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Student";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(917, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.AgeBox);
+            this.Controls.Add(this.DayBox);
+            this.Controls.Add(this.NameBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.TextBox DayBox;
+        private System.Windows.Forms.TextBox AgeBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFromsApp
+{
+    public partial class Form1 : Form
+    {
+        public enum WorkDays
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str = (NameBox.Text + " " + DayBox.Text + " " + AgeBox.Text);
+
+            StreamWriter sr = File.AppendText(@"C:\Users\shaha\source\repos\WindowsFromsApp\WindowsFromsApp\FormTask.txt");
+
+            sr.WriteLine(str);
+
+            sr.Close();
+
+            NameBox.Text = "";
+            DayBox.Text = "";
+            AgeBox.Text = "";
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            WorkDays fromString;
+            string[] str;
+            string[] strBox = new string[3];
+            string fileName = @"C:\Users\shaha\source\repos\WindowsFromsApp\WindowsFromsApp\FormTask.txt";
+            str = File.ReadAllLines(fileName);
+
+            strBox[0] = str[0].Split(' ')[0];
+            strBox[1] = str[0].Substring(strBox[0].Length+1).Split(' ')[0];
+            strBox[2] = str[0].Substring(strBox[0].Length + strBox[1].Length+2).Split(' ')[0];
+
+            NameBox.Text = strBox[0];
+            DayBox.Text = strBox[1];
+            AgeBox.Text = strBox[2];
+
+           // fromString = (WorkDays)Enum.Parse(typeof(WorkDays), strBox[1]);
+        }
+        private void NameBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            NameBox.Text = "";
+        }
+
+        private void DayBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            DayBox.Text = "";
+        }
+
+        private void AgeBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            AgeBox.Text = "";
+        }
+    }
+    
+}
